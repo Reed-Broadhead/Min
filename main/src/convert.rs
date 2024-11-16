cdd834ca9c4134124a3655e9f871a41a0efffd0a
Reed <reedbroadhead@gmail.com>
2024-11-15 20:43:53 -0600

    complets fix

--- main/src/convert.rs
+++ main/src/convert.rs
@@ -12,35 +12,7 @@ pub struct ImgFile {
     pub quality: f32,
 }
 impl ImgFile {
-   // pub fn to_webp(&self) {
-   //     println!("Converting to PNG {:?}", &self.path);
 
-   //     let img_results = ImageReader::open(&self.path).unwrap().decode();
-
-   //     let img = match img_results{
-   //        Ok(file) => file,
-   //        Err(_) => {println!("Could not convert: {:?}", &self.path); return } 
-   //     };
-
-   //     let encoder_results = Encoder::from_image(&img);
-
-   //     let encoder = match encoder_results {
-   //         Ok(file) => file,
-   //         Err(_) => {println!("Could not convert: {:?}", &self.path); return }
-   //     }; 
-
-   //     let webp: WebPMemory = encoder.encode(self.quality);
-
-   //     let output_path = Path::new("").join(&self.name).with_extension("webp");
-
-   //     let _ = if self.replace == true {
-   //          std::fs::remove_file(&self.path)
-   //     } else {Ok(())};
-   //     
-   //     std::fs::write(&output_path, &*webp).unwrap();
-   //     println!("Saved to {:?}", output_path);
-
-   // }
     pub fn new_to_webp(&self){
 
         println!("Converting to PNG {:?}", &self.path);
